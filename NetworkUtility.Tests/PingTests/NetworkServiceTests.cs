dfaf60a051e01e4002b92a10a63d556cdf9f02a8
Jinhyuk <kobin1970@gmail.com>
2024-10-29 18:36:37 +0100

    add test

--- NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -1,9 +1,22 @@
+using FluentAssertions;
+using NetworkUtility.Ping;
+using Xunit;
 namespace NetworkUtility.Tests.PingTests;
 
 public class NetworkServiceTests
 {
+    [Fact]
     public void NetworkService_SendPing_ReturnString()
     {
-        
+        // Arrange - Variables,Classes,Mocks
+        var pingService = new NetworkService();
+        // Act
+        var result = pingService.SendPing();
+        // Assert
+        string expected = "pong";
+        Assert.Equal(expected, result);
+        result.Should().NotBeNull();
+        // result.Should().Be("ping");
+
     }
 }
--- NetworkUtility.Tests/obj/Debug/net8.0/NetworkUtility.Tests.AssemblyInfo.cs
+++ NetworkUtility.Tests/obj/Debug/net8.0/NetworkUtility.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetworkUtility.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c98624d028a6b50e18deebd84522a0d324dc114a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetworkUtility.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetworkUtility.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
